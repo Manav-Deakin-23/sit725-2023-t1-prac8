3da19219a0d8649538e2886a42e25db134be4358
Manav <s223608412@deakin.edu.au>
2023-08-29 18:11:42 +1000

    socket

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ let app = express();
 let port = process.env.port || 3000;
 require('./database_connection');
 let router = require("./routers/router");
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
@@ -11,7 +15,17 @@ app.use(express.urlencoded({
 }));
 app.use('/api/cat', router);
 
+io.on('connection', (socket) => {
+console.log('a rocket is connected');
+socket.on('disconnect', () => {
+console.log('user disconnected');
+});
+setInterval(()=>{
+socket.emit('number', parseInt(Math.random()*10));
+}, 1000);
+});
 
-app.listen(port, () => {
+
+http.listen(port, () => {
     console.log('express server started');
 });
